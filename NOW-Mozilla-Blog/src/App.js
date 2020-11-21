1bf610a69e6e059d767cf97e97a0866ad4db72c1
Aajinkya Singh <43881544+aajinkya1203@users.noreply.github.com>
2020-11-21 14:35:36 +0530

    Added backend stuff

--- NOW-Mozilla-Blog/src/App.js
+++ NOW-Mozilla-Blog/src/App.js
@@ -3,6 +3,10 @@ import LandingPage from './layout/LandingPage'
 import HomePage from './layout/HomePage'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import ArticleDetail from './components/ArticleDetail'
+// fcfb165ebb74b13b955eabeedec2b3df911fc038
+
+// encoded -
+// ZmNmYjE2NWViYjc0YjEzYjk1NWVhYmVlZGVjMmIzZGY5MTFmYzAzOA==
 
 function App() {
   return (
