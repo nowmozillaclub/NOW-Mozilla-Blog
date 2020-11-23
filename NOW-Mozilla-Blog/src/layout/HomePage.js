733f3a090d0bb160c5eb415ec7cd85f218f42c50
Aajinkya Singh <43881544+aajinkya1203@users.noreply.github.com>
2020-11-23 15:22:30 +0530

    Made it all responsive now

--- NOW-Mozilla-Blog/src/layout/HomePage.js
+++ NOW-Mozilla-Blog/src/layout/HomePage.js
@@ -52,6 +52,9 @@ const HomePage = () => {
         let tempAnother = document.createElement('script');
         tempAnother.src = "//cdn.jsdelivr.net/github-cards/latest/widget.js";
         document.getElementById("github-embed").appendChild(tempAnother);
+        window.$(document).ready(function(){
+            window.$('.sidenav').sidenav();
+          });
         if (!loading) {
             if (error) {
                 console.error(error)
@@ -63,92 +66,107 @@ const HomePage = () => {
         }
     }, [loading, error, data]);
     return (
-        <div>
-            <div className="row">
-                <div className="col s12 m3 sideNav section hide-on-med-and-down" style={{position:"fixed"}}>
-                    <div className="section">
-                        <img src={require("../images/now-square.png")} className="circle responsive-img sideNavPP" />
-                        <h5>{new Date().toDateString()}</h5>
-                        <p className="grey-text">{new Date().toLocaleTimeString()}</p>
-                    </div>
-                    <div className="section">
-                        <div className="row">
-                            <div className="col s12 valign-wrapper" style={{margin:"10px 0"}}>
-                                <Link to='/' className="black-text valign-wrapper flow-text"><i class="material-icons navIcony">home</i><span>Home</span></Link>
-                            </div>
-                            <div className="col s12 valign-wrapper" style={{margin:"10px 0"}}>
-                                <Link to='/homepage' className="black-text valign-wrapper flow-text"><i class="material-icons navIcony">book</i><span>Blogs</span></Link>
-                            </div>
-                            <div className="col s12 valign-wrapper" style={{margin:"10px 0"}}>
-                                <Link to='/https://nowmozilla.club' className="black-text valign-wrapper flow-text"><i class="material-icons navIcony">info_outline</i><span>About NOW</span></Link>
-                            </div>
+        <>
+            <div>
+                <div className="row">
+                    
+                    <div className="col s12 m3 sideNav section hide-on-med-and-down" style={{position:"fixed"}}>
+                        <div className="section">
+                            <img src={require("../images/now-square.png")} className="circle responsive-img sideNavPP" />
+                            <h5>{new Date().toDateString()}</h5>
+                            <p className="grey-text">{new Date().toLocaleTimeString()}</p>
                         </div>
-                        <div className="section hide-on-med-and-down" id="github-embed">
-                            {/* <div className="row">
-                                <div className="col s10 cardWrapper">
-                                    <div className="card sideNavCard">
-                                        <div className="card-content white-text">
-                                            <span className="card-title">Card Title</span>
-                                            <p></p>
-                                        </div>
-                                        <div className="card-action sideNavCard">
-                                            <a href="#">This is a link</a>
+                        <div className="section">
+                            <div className="row">
+                                <div className="col s12 valign-wrapper" style={{margin:"10px 0"}}>
+                                    <Link to='/' className="black-text valign-wrapper flow-text"><i className="material-icons navIcony">home</i><span>Home</span></Link>
+                                </div>
+                                <div className="col s12 valign-wrapper" style={{margin:"10px 0"}}>
+                                    <Link to='/homepage' className="black-text valign-wrapper flow-text"><i className="material-icons navIcony">book</i><span>Blogs</span></Link>
+                                </div>
+                                <div className="col s12 valign-wrapper" style={{margin:"10px 0"}}>
+                                    <Link to='/https://nowmozilla.club' className="black-text valign-wrapper flow-text"><i className="material-icons navIcony">info_outline</i><span>About NOW</span></Link>
+                                </div>
+                            </div>
+                            <div className="section hide-on-med-and-down" id="github-embed">
+                                {/* <div className="row">
+                                    <div className="col s10 cardWrapper">
+                                        <div className="card sideNavCard">
+                                            <div className="card-content white-text">
+                                                <span className="card-title">Card Title</span>
+                                                <p></p>
+                                            </div>
+                                            <div className="card-action sideNavCard">
+                                                <a href="#">This is a link</a>
+                                            </div>
                                         </div>
                                     </div>
+                                </div> */}
+                                <div className="github-card" 
+                                    style={{
+                                        maxWidth:"100%",
+                                        height:"auto"
+                                    }}
+                                    data-github="nowmozillaclub" data-height="auto" data-width="90%" data-theme="default">
                                 </div>
-                            </div> */}
-                            <div className="github-card" 
-                                style={{
-                                    maxWidth:"100%",
-                                    height:"auto"
-                                }}
-                                data-github="nowmozillaclub" data-height="auto" data-width="90%" data-theme="default">
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col m12 l9 offset-l3">
-                    <div className="row">
-                        <div className="col s12">
-                            <div className="row" style={{paddingTop: "20px"}}>
-                                <div className="col s12 center-align l5">
-                                    <h5>NOW Mozilla Blogs</h5>
+                    <div className="col s12 m12 l9 offset-l3" style={{padding:"0"}}>
+                        {/* <div className="row"> */}
+                            <nav className="show-on-medium-and-down col s12" id="mobiley" style={{display:"none", background: "#ffb94a"}}>
+                                <div className="nav-wrapper">
+                                    <a href="#!" className="brand-logo left valign-wrapper" style={{height:"100%"}}><img src={require('../images/NOWImage.jpeg')} style={{height:"100%", padding: "5px"}} alt="NOW Blogs Logo"/></a>
+                                    <a href="#" data-target="mobile-demo" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
                                 </div>
-                                <div className="col s10 offset-s1 m8 offset-m2 center-align l5" id="searchWrapper">
-                                    <nav className="searchBar">
-                                        <div className="nav-wrapper searchBar">
-                                            <form>
-                                                <div className="input-field">
-                                                    <i className="material-icons prefix black-text" style={{top:0}}>search</i>
-                                                    <input id="search" type="text" style={{borderRadius: "24px", height:"100%"}} className="validate" />
-                                                    <label htmlFor="search" style={{height:"100%", top:"-10px"}}>Search for something</label>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </nav>
-                                </div>
+                            </nav>
+
+                            <ul className="sidenav" id="mobile-demo">
+                                <li><Link to='/' className="black-text valign-wrapper flow-text"><span>Home</span></Link></li>
+                                <li><Link to='/homepage' className="black-text valign-wrapper flow-text"><span>Blogs</span></Link></li>
+                                <li><Link to='/https://nowmozilla.club' className="black-text valign-wrapper flow-text"><span>About NOW</span></Link></li>
+                            </ul>
+                            <div className="col s12" style={{paddingTop: "20px"}}>
+                                {/* <div className="row"> */}
+                                    <div className="col s12 center-align l5">
+                                        <h5>NOW Mozilla Blogs</h5>
+                                    </div>
+                                    <div className="col s10 offset-s1 m8 offset-m2 center-align l5" id="searchWrapper">
+                                        <nav className="searchBar">
+                                            <div className="nav-wrapper searchBar">
+                                                <form>
+                                                    <div className="input-field">
+                                                        <i className="material-icons prefix black-text" style={{top:0}}>search</i>
+                                                        <input id="search" type="text" style={{borderRadius: "24px", height:"100%"}} className="validate" />
+                                                        <label htmlFor="search" className="truncate" style={{height:"100%", top:"-10px"}}>Search for something</label>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </nav>
+                                    </div>
+                                {/* </div> */}
                             </div>
-                        </div>
-                        <div className="col s12 section2">
-                            <div className="row">
-                                <div className="col s12">
-                                    {
-                                        posts.length < 3 ? <ScrollCard blog={posts} /> : <ScrollCard blog={posts.slice(0, 3)} />
-                                    }
-                                    {
-                                        loading
-                                            ? <Loader />
-                                            : posts.map((v, i) => {
-                                                return <Card2 blog={v} key={i} />;
-                                            })
-                                    }
-                                </div>
+                            <div className="col s12 section2">
+                                {/* <div className="row"> */}
+                                    {/* <div className="col s12"> */}
+                                        {
+                                            posts.length < 3 ? <ScrollCard blog={posts} /> : <ScrollCard blog={posts.slice(0, 3)} />
+                                        }
+                                        {
+                                            loading
+                                                ? <Loader />
+                                                : posts.map((v, i) => {
+                                                    return <Card2 blog={v} key={i} />;
+                                                })
+                                        }
+                                    {/* </div> */}
+                                {/* </div> */}
                             </div>
-                        </div>
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
