15c52eab72eaa3b73064f79ab0be0d3698b5507e
Pratit23 <51255935+Pratit23@users.noreply.github.com>
2020-11-25 19:12:00 +0530

    Updates

---
+++ precache-manifest.f3c1ebd4575a0c86d8ab502e18987749.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a58ceab2ef354253ad3de5b8d76fb52",
+    "url": "/NOW-Mozilla-Blog/index.html"
+  },
+  {
+    "revision": "b0466169da68ebf4de75",
+    "url": "/NOW-Mozilla-Blog/static/css/main.6cd34156.chunk.css"
+  },
+  {
+    "revision": "4f36e29b0f5c51a69ada",
+    "url": "/NOW-Mozilla-Blog/static/js/2.14d819cb.chunk.js"
+  },
+  {
+    "revision": "ec308ff646b9a1804bb84134a7674681",
+    "url": "/NOW-Mozilla-Blog/static/js/2.14d819cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0466169da68ebf4de75",
+    "url": "/NOW-Mozilla-Blog/static/js/main.c1a96833.chunk.js"
+  },
+  {
+    "revision": "f2a15a5942c6b1b67ad5",
+    "url": "/NOW-Mozilla-Blog/static/js/runtime-main.725c86e1.js"
+  },
+  {
+    "revision": "7e00c4e98961e75926084fc4535b6196",
+    "url": "/NOW-Mozilla-Blog/static/media/Banner.7e00c4e9.jpeg"
+  },
+  {
+    "revision": "980c66c2eef964f397e480e62285f589",
+    "url": "/NOW-Mozilla-Blog/static/media/NOWImage.980c66c2.jpeg"
+  },
+  {
+    "revision": "7d418a1e16be0f2540ade24ffb34e976",
+    "url": "/NOW-Mozilla-Blog/static/media/Subscribe.7d418a1e.svg"
+  },
+  {
+    "revision": "a1449f8bfc552b5c75c6617d14947a38",
+    "url": "/NOW-Mozilla-Blog/static/media/WhyBlogs.a1449f8b.svg"
+  },
+  {
+    "revision": "2f01f6c9a587820f5c9fed186942a906",
+    "url": "/NOW-Mozilla-Blog/static/media/gradient.2f01f6c9.jpg"
+  },
+  {
+    "revision": "20ee677b67d9da5203aba53b8df6c9f8",
+    "url": "/NOW-Mozilla-Blog/static/media/now-circle.20ee677b.png"
+  },
+  {
+    "revision": "af75154f3b29d1b1cdfbc415048b1460",
+    "url": "/NOW-Mozilla-Blog/static/media/pp.af75154f.PNG"
+  },
+  {
+    "revision": "706b1efd6e40538c7545f9ce33c72071",
+    "url": "/NOW-Mozilla-Blog/static/media/splash.706b1efd.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NOW-Mozilla-Blog/precache-manifest.e76133aca5388abf364d349625f3bf48.js"
+  "/NOW-Mozilla-Blog/precache-manifest.f3c1ebd4575a0c86d8ab502e18987749.js"
 );
 
 self.addEventListener('message', (event) => {
